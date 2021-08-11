a4f668cfdbf9a18f0cb590b575c353209fa73410
beauxq <beauxq@users.noreply.github.com>
2021-08-11 14:42:14 -0700

    init

---
+++ README.txt
@@ -0,0 +1,29 @@
+=====================================
+an example currency conversion api
+=====================================
+
+gets rate information from:
+https://freecurrencyapi.net/
+
+
+Setup
+=====
+
+Assuming you have a working Docker installation, you should be able to just run:
+
+docker-compose up
+
+to start the app. It should be accessible on port 5000 of your docker machine's IP address.
+
+features
+========
+
+- `/rate` a GET endpoint to get a currency rate
+    - Returns the conversion rate from `from` to `to` as a floating point number.
+    - The rate is the value of 1 unit of `from` in `to`.
+    - The return value is a JSON object restating the request parameters and the rate.
+
+- `/convert` a GET endpoint that converts a value in one currency to another
+    - returns an amount in one currency, converted to an amount in another currency.
+    - The return value is a JSON object restating the request parameters and the converted amount.
+    - The converted amount is rounded to 2 decimal points.
---
+++ app.py
@@ -0,0 +1,129 @@
+from typing import Dict, Tuple, Union
+from requests import get
+from time import time
+from flask import Flask, jsonify, Response, request, make_response, Request
+
+
+app = Flask(__name__)
+
+class RateCache:
+    currencies = {
+        "USD",
+        "GBP",
+        "EUR"
+    }
+
+    def __init__(self) -> None:
+        self._rates = {c: 0.0 for c in RateCache.currencies}
+        self._last_update = 0.0
+        self._valid = False
+
+    def _update_rates(self) -> bool:
+        """
+        updates rates from freecurrencyapi.net
+
+        returns whether rates are valid values received from freecurrencyapi.net
+        """
+        now = time()
+        # "You are allowed to submit 10 requests per hour without authentication."
+        if now - self._last_update > 420:
+            self._last_update = now
+            print("checking for update to exchange rates")
+            res = get("https://freecurrencyapi.net/api/v1/rates")
+            if res.status_code == 200:
+                try:
+                    values: Dict[str, float] = next(iter(res.json()['data'].values()))
+                    for currency in RateCache.currencies:
+                        if currency == "EUR":
+                            # this freecurrencyapi.net uses EUR as the base
+                            self._rates[currency] = 1.0
+                            continue    
+                        if type(values[currency]) is not float:
+                            raise TypeError("value for currency wasn't float")
+                        self._rates[currency] = values[currency]
+                    self._valid = True
+                except (TypeError, KeyError) as e:
+                    print("freecurrencyapi.net didn't give expected data format")
+                    print(e)
+                    print(res.json())
+                    self._valid = False
+            else:
+                print("freecurrencyapi.net didn't give status code 200")
+                print(res.status_code)
+                print(res.content)
+                # valid doesn't change
+        return self._valid
+    
+    def rate(self, from_code: str, to_code: str) -> float:
+        """
+        returns the conversion rate
+
+        returns 0.0 if the rate is not available
+        (bad codes or server not available)
+        """
+        if (from_code in RateCache.currencies
+        and to_code in RateCache.currencies
+        and self._update_rates()):
+            return self._rates[to_code] / self._rates[from_code]
+        else:
+            return 0.0
+
+rate_cache = RateCache()
+            
+
+@app.route('/test')
+def test() -> Response:
+    return jsonify({'message': 'test successful'})
+
+
+@app.route('/test/<string:name>')
+def test_name(name: str) -> Response:
+    return jsonify({'message': 'test successful for {}'.format(name)})
+
+
+def get_rate(request: Request) -> Tuple[float, Union[Response, Dict[str, Union[str, float]]]]:
+    """
+    returns the rate and a dictionary that can be jsonified to a response
+
+    if there's a problem, returns 0.0 and the problem response
+    """
+    from_param = request.args.get('from')
+    to_param = request.args.get('to')
+    if from_param is None or to_param is None:
+        return 0.0, make_response("this endpoint requires `from` and `to` parameters", 400)
+    from_param = from_param.upper()
+    to_param = to_param.upper()
+    if from_param not in RateCache.currencies or to_param not in RateCache.currencies:
+        return 0.0, make_response(f"valid currencies are {RateCache.currencies}", 400)
+    rate = rate_cache.rate(from_param, to_param)
+    if rate == 0.0:
+        return 0.0, make_response("unable to get exchange rates", 503) 
+    return rate, {
+        "from": from_param,
+        "to": to_param,
+        "rate": rate
+    }
+
+
+@app.route('/rate')
+def rate() -> Response:
+    _, res = get_rate(request)
+    if isinstance(res, Response):
+        return res
+    else:
+        return jsonify(res)    
+
+
+@app.route('/convert')
+def convert() -> Response:
+    rate, res = get_rate(request)
+    if isinstance(res, Response):
+        return res
+
+    amount = request.args.get('amount', type=float)
+    if amount is None:
+        return make_response("this endpoint requires `from` and `to` and `amount` parameters", 400)
+    
+    res["amount"] = amount
+    res["converted"] = round(amount * rate, 2)
+    return jsonify(res)
